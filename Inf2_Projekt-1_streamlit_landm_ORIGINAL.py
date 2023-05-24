aa819ad13bcfeb708e3b64a69c5b2fe4060770e6
landolfom <133596322+landolfom@users.noreply.github.com>
2023-05-24 18:11:37 +0200

    Update Inf2_Projekt-1_streamlit_landm_ORIGINAL.py

--- Inf2_Projekt-1_streamlit_landm_ORIGINAL.py
+++ Inf2_Projekt-1_streamlit_landm_ORIGINAL.py
@@ -467,28 +467,26 @@ with tab5:
         if search_button: 
             search_and_display_row(df1, 'Datum/Zeit')
             delete_button = st.button('Gefundene Daten löschen')
-            if delete_button:
-                value = input6
-                matching_rows = df1[df1["Datum/Zeit"].str.contains(value)]
+        if delete_button:
+            value = input6
+            matching_rows = df1[df1["Datum/Zeit"].str.contains(value)]
     
-                if len(value) == 0:
-                    st.warning("Keine Parameter eingegeben!")
-                elif len(matching_rows) == 0:
-                    st.warning("Es wurden keine Parameter entsprechend der Sucheeingabe gefunden! Somit können keine Daten gelöscht werden.")
-                elif len(value) > 0:
-                    st.text('Sollen die Daten wirklich gelöscht werden?')
-                    JA_button=st.button('JA')
-                    NEIN_button=st.button('NEIN')
-                    if JA_button:
-                        df1 = df1.drop(matching_rows.index)
-                        json_data = df1.to_json(orient='records')
-                        json_dict = json.loads(json_data)
-                        save_data(json_dict)
-                        st.success("Parameter wurden erfolgreich gelöscht.")
-                    elif NEIN_button:
-                        st.success("Parameter werden NICHT gelöscht und verbleiben im System.")             
-                else:
-                    st.error("Es ist etwas schief gelaufen!")
+            if len(value) == 0:
+                st.warning("Keine Parameter eingegeben!")
+            elif len(matching_rows) == 0:
+                st.warning("Es wurden keine Parameter entsprechend der Sucheeingabe gefunden! Somit können keine Daten gelöscht werden.")
+            elif len(value) > 0:
+                st.text('Sollen die Daten wirklich gelöscht werden?')
+                JA_button=st.button('JA')
+                NEIN_button=st.button('NEIN')
+        if JA_button:
+            df1 = df1.drop(matching_rows.index)
+            json_data = df1.to_json(orient='records')
+            json_dict = json.loads(json_data)
+            save_data(json_dict)
+            st.success("Parameter wurden erfolgreich gelöscht.")
+        if NEIN_button:
+            st.success("Parameter werden NICHT gelöscht und verbleiben im System.")             
  
     
     # Laden der JSON-Daten
